405b766931dbc33f67733eb9241d569bba39f727
Saravana-kumar-j <saravanakumarj.net@gmail.com>
2024-03-07 23:33:27 +0530

    Updated PHP

--- login.php
+++ login.php
@@ -1,42 +1,44 @@
 <?php
-    include ("database.php");
+    include("database.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Page</title>
+    <title>Login Page</title>
 </head>
 <body>
-    <form action="index.php" method="post">
-    <h2>Registration form for Book list.</h2>
+    <form action="login.php" method="post">
+    <h2>Login Page</h2>
     Username: <br>
     <input type="text" name="username"><br>
     Password: <br>
     <input type="password" name="password"><br>
-    <input type="submit" name="submit" value="Register">
+    <input type="submit" name="submit" value="Login">
     </form>
 </body>
 </html>
 <?php
     $username = $_POST["username"];
     $password = $_POST["password"];
-
+    
     if(empty($username)){
         echo"Please enter a Username.";
     } elseif(empty($password)){
         echo"Please enter a Password.";
     } else {
-        $checkuser = "SELECT * FROM user WHERE user_name = '$username'";
+        $sql = "SELECT * FROM user WHERE user_name = '$username'";
+        
         try{
-            $res_checkuser = mysqli_query($conn, $checkuser);
-            if(mysqli_num_rows($res_checkuser) > 0){
-                echo"User already exists. Please try another username.";
+            $result = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($result)>0){
+                while($row =mysqli_fetch_assoc($result)){
+                    header("Location: welcome.php?username=" . $username);
+                    exit();
+                };
             } else {
-                $insertquery = "INSERT INTO user (user_name, password) VALUES ('$username','$password')";
-                mysqli_query($conn, $insertquery);
-                echo"You'Registered succesfully.";
+                echo"Your Data Doen't exists. Please Register before login.";
             }
         }
         catch(mysqli_sql_exception){
