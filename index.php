bafcf2c796a06f0e910093dee1c07f1f9ac277e9
Saravana-kumar-j <saravanakumarj.net@gmail.com>
2024-03-07 22:09:19 +0530

    Updated simple PHP Script

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <title>Registration Page</title>
 </head>
 <body>
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+    <form action="index.php" method="post">
     <h2>Registration form for Book list.</h2>
     Username: <br>
     <input type="text" name="username"><br>
@@ -20,23 +20,21 @@
 </body>
 </html>
 <?php
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
-        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-        if(empty($username)){
-            echo"Please enter a Username.";
-        } elseif(empty($password)){
-            echo"Please enter a Password.";
-        } else {
-            $sql = "INSERT INTO user (user_name, password) VALUES ('$username','$password')";
-            try{
-                mysqli_query($conn, $sql);
-                echo"You are registered now.";
-            }
-            catch(mysqli_sql_exception){
-                echo"Error Occured.";
-            }
+    if(empty($username)){
+        echo"Please enter a Username.";
+    } elseif(empty($password)){
+        echo"Please enter a Password.";
+    } else {
+        $sql = "INSERT INTO user (user_name, password) VALUES ('$username','$password')";
+        try{
+            mysqli_query($conn, $sql);
+            echo"You are registered now.";
+        }
+        catch(mysqli_sql_exception){
+            echo"Error Occured.";
         }
     }
     mysqli_close($conn);
